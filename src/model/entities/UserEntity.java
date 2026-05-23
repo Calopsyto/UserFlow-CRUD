99c9f3dac1877e08c62abe16cc0553cbf7df1676
Arthur <arthusantosfernandes337@gmail.com>
2026-05-23 19:42:45 -0300

    feat: add userEntity and getter/setter

---
+++ src/model/entities/UserEntity.java
@@ -0,0 +1,33 @@
+package model.entities;
+
+import java.sql.Date;
+
+public class UserEntity {
+    private String name;
+    private Integer age;
+    private Date birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
