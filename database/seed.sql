90f2b4becfd3232253e073c518e282985ea442be
Arthur <arthusantosfernandes337@gmail.com>
2026-05-23 21:06:34 -0300

    feat: create sql scripts

---
+++ database/seed.sql
@@ -0,0 +1,13 @@
+USE userflow_db;
+
+INSERT INTO User (Name, Email, Age, BirthDate)
+VALUES
+    ('Arthur Souza', 'arthur@gmail.com', 17, '2008-03-15'),
+
+    ('Sury Costa', 'sury@gmail.com', 25, '199912-09'),
+
+    ('Lucas Lima', 'lucas@gmail.com', 31, '1993-07-21'),
+
+    ('Marina Alves', 'marina@gmail.com', 22, '2002-01-10'),
+
+    ('Fernanda Rocha', 'fernanda@gmail.com', 28, '1996-05-30');
